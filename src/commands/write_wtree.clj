0cd988f02a1ca0852f0c23948cf39c5492f7d10c
Marc Lewis <marc8@live.unc.edu>
2020-03-27 18:57:38 -0400

    Remove writing for empty dirs and already hashed blobs

--- src/commands/write_wtree.clj
+++ src/commands/write_wtree.clj
@@ -16,30 +16,50 @@
          hash-and-store-tree-object)
 
 (defn write-entry [dir dbase f] ; returns byte array, processes entry type and passes to appropriate fn
-  (if (.isDirectory (io/file (str dir File/separator f)))
+  (cond
+    (and (.isDirectory (io/file (str dir File/separator f))) (not (empty? (.list (io/file (str dir File/separator f)))))) ; filter out empty directories
     (do
       ;(println "Making a tree entry!")
       (dir-to-tree-entry (str dir File/separator f) (str ".." File/separator dbase)))
+    (not (.isDirectory (io/file (str dir File/separator f))))
     (do
       ;(println "Making a blob entry!")
       (write-blob-object-and-return-blob-entry dir dbase f)) ;else
+    :else
+    nil
     )
   )
 
 (defn write-blob-object-and-return-blob-entry [dir dbase file]
-  (let [mode "100644"
-        path dir
-        address (hash/hash-object dir dbase (list "-w" file))
-        entry (str mode " " path "\000" address)]
-    ;(println entry) ; just for my sanity, not to keep
-    ;(println (.getBytes entry))
-    (.getBytes entry) ; return the bytes of the entry, for concatenation usage
+  (let [hash (hash/hash-object dir dbase (list file))
+        first2 (subs hash 0 2)
+        last38 (subs hash 2)
+        alreadythere (.exists (io/file (str dir File/separator dbase File/separator "objects" File/separator first2 File/separator last38)))
+        ]
+
+    (if alreadythere
+      (let [mode "100644"
+            path dir
+            address (hash/hash-object dir dbase (list file)) ; skip writing when file is already there
+            entry (str mode " " path "\000" address)]
+        ;(println "Didn't write to dbase")
+        (.getBytes entry) ; return the bytes of the entry, for concatenation usage
+        )
+      ; else
+      (let [mode "100644"
+            path dir
+            address (hash/hash-object dir dbase (list "-w" file)) ; do write when file is not already there
+            entry (str mode " " path "\000" address)]
+        ;(println "Wrote to dbase")
+        (.getBytes entry) ; return the bytes of the entry, for concatenation usage
+        )
+      )
     )
   )
 
 (defn dir-to-tree-entry [dir dbase]
   (let [address (dir-to-tree-object-contents dir dbase)
-        mode "040000"
+        mode "40000"
         path dir
         entry (str mode " " path "\000" address)]
     ;(println entry)
@@ -53,22 +73,24 @@
                        (filter (fn [f] (not (.contains f dbase)))) ; filter out the database and all its children
                        )
         ]
-    ;(println "Location of dbase: " dbase)
-    ;(println "Contents of " dir " directory:") TESTING MATERIAL
-    ;(pprint str-all)
 
-    (let [str-bytes-all (map (fn [f] (write-entry dir dbase f)) str-all) ; should be sequence of byte arrays
-          str-bytes-concat (apply concat str-bytes-all) ; SHOULD BE just one byte array, but its still a lazy sequence
-          header (str "tree" (count str-bytes-concat) "\000") ; header
+    ;(println "Location of dbase: " dbase)
+    ;(println "Contents of " dir " directory:") ;TESTING MATERIAL
+    ;(pprint str-all)
+    ;(println)
+
+    (let [str-bytes-all (map (fn [f] (write-entry dir dbase f)) str-all) ; should be sequence of byte arrays, with some nils from empty dirs and stuff
+          ;str-bytes-no-nil (filter (fn [f] (not (nil? f))) str-bytes-all) ; the nils that came back from unsuccessful calls are skipped
+          str-bytes-concat (apply concat str-bytes-all) ; should concatenate all bytes together, skipping nils
+          header (str "tree" " " (count str-bytes-concat) "\000") ; header
           header-bytes (.getBytes header) ; make a byte array from that
           tree-bytes (byte-array (concat header-bytes str-bytes-concat)) ; should put header bytes on front of concat byte array
           ]
 
       ;(println tree-bytes) ; remove later, just for testing
-      (if (= 0 (count all)) ; didn't work
-             nil
-             (hash-and-store-tree-object dir dbase tree-bytes)
-             )
+
+      (hash-and-store-tree-object dir dbase tree-bytes)
+
       ;(hash-and-store-tree-object dir dbase tree-bytes); return
       )
   ))
