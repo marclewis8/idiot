e083cf29c3c5a78a37ba3d0c8e3ea40e05f46ce7
Marc Lewis <marc8@live.unc.edu>
2020-03-30 11:26:49 -0400

    Changes to write-wtree

--- src/commands/write_wtree.clj
+++ src/commands/write_wtree.clj
@@ -4,7 +4,7 @@
             [commands.utils.tools :as tools]
             [clojure.pprint :refer :all]
             [commands.hash-object :as hash]
-            )
+            [clojure.string :as str])
 
   (:import java.io.File)
   )
@@ -19,14 +19,17 @@
   (cond
     (and (.isDirectory (io/file (str dir File/separator f))) (not (empty? (.list (io/file (str dir File/separator f)))))) ; filter out empty directories
     (do
-      ;(println "Making a tree entry!")
+      (println "Making a tree entry for " f "!")
       (dir-to-tree-entry (str dir File/separator f) (str ".." File/separator dbase)))
     (not (.isDirectory (io/file (str dir File/separator f))))
     (do
-      ;(println "Making a blob entry!")
+      (println "Making a blob entry for " f "!")
       (write-blob-object-and-return-blob-entry dir dbase f)) ;else
     :else
-    nil
+    (do
+      (println "Skipping making a tree entry for empty directory " f ".\n")
+      nil
+      )
     )
   )
 
@@ -39,18 +42,20 @@
 
     (if alreadythere
       (let [mode "100644"
-            path dir
+            path (str file)
             address (hash/hash-object dir dbase (list file)) ; skip writing when file is already there
             entry (str mode " " path "\000" address)]
-        ;(println "Didn't write to dbase")
+        (println "Didn't write to dbase")
+        (println entry "\n")
         (.getBytes entry) ; return the bytes of the entry, for concatenation usage
         )
       ; else
       (let [mode "100644"
-            path dir
+            path (str file)
             address (hash/hash-object dir dbase (list "-w" file)) ; do write when file is not already there
             entry (str mode " " path "\000" address)]
-        ;(println "Wrote to dbase")
+        (println "Wrote to dbase")
+        (println entry "\n")
         (.getBytes entry) ; return the bytes of the entry, for concatenation usage
         )
       )
@@ -58,42 +63,47 @@
   )
 
 (defn dir-to-tree-entry [dir dbase]
-  (let [address (dir-to-tree-object-contents dir dbase)
+  (let [address (tools/sha-bytes (dir-to-tree-object-contents dir dbase))
         mode "40000"
-        path dir
+        fullpath dir ; returns a path from dir write-wtree was called on
+        pathlist (str/split fullpath (re-pattern (str "\\" File/separator))) ; splits path into individual dir name chunks
+        path (nth pathlist (- (count pathlist) 1)) ; takes last path (what i want)
         entry (str mode " " path "\000" address)]
-    ;(println entry)
+
+    (println entry)
     (.getBytes entry)
-  ))
+    ))
 
 (defn dir-to-tree-object-contents [dir dbase]
   (let [all (.list (io/file dir))
         str-all (->> all
-                       (map str) ; turn them all into strings, should remove java.io.file nonsense
-                       (filter (fn [f] (not (.contains f dbase)))) ; filter out the database and all its children
-                       )
+                     (map str) ; turn them all into strings, should remove java.io.file nonsense
+                     (filter (fn [f] (not (.contains f dbase)))) ; filter out the database and all its children
+                     )
         ]
 
-    ;(println "Location of dbase: " dbase)
-    ;(println "Contents of " dir " directory:") ;TESTING MATERIAL
-    ;(pprint str-all)
-    ;(println)
+    (println "Location of dbase: " dbase)
+    (println "Contents of " dir " directory:") ;TESTING MATERIAL
+    (pprint str-all)
+    (print "\n")
 
     (let [str-bytes-all (map (fn [f] (write-entry dir dbase f)) str-all) ; should be sequence of byte arrays, with some nils from empty dirs and stuff
           ;str-bytes-no-nil (filter (fn [f] (not (nil? f))) str-bytes-all) ; the nils that came back from unsuccessful calls are skipped
           str-bytes-concat (apply concat str-bytes-all) ; should concatenate all bytes together, skipping nils
-          header (str "tree" " " (count str-bytes-concat) "\000") ; header
+          header (str "tree" " " (count str-bytes-concat) "\000") ; header (count str-bytes-concat)
           header-bytes (.getBytes header) ; make a byte array from that
           tree-bytes (byte-array (concat header-bytes str-bytes-concat)) ; should put header bytes on front of concat byte array
           ]
-
+      ;(ByteArrayOutputStream.) nah
       ;(println tree-bytes) ; remove later, just for testing
+      (let [tree-readable (apply str (map char tree-bytes))]
+        (println tree-readable)
+        )
+      (hash-and-store-tree-object dir dbase tree-bytes) ; stores tree object, returns the hash for printing / entry-making
+      tree-bytes
 
-      (hash-and-store-tree-object dir dbase tree-bytes)
-
-      ;(hash-and-store-tree-object dir dbase tree-bytes); return
       )
-  ))
+    ))
 
 
 
@@ -105,7 +115,7 @@
     (let [zipped-tree (tools/zip-str tree-bytes)]
       (io/copy zipped-tree (io/file (str dir File/separator dbase File/separator "objects" File/separator first2 File/separator last38)))
       )
-    tree-hex ; return hash to print
+    tree-hex ; return hash to print / use for address
     )
   )
 ;TODO make function that takes in contents, and hashes / stores it in dbase
@@ -116,5 +126,5 @@
     (not= args nil) (println "Error: write-wtree accepts no arguments")
     (not (.isDirectory (io/file (str dir File/separator dbase)))) (println "Error: could not find database. (Did you run `idiot init`?)")
     (= 1 (count (.list (io/file dir)))) (println "The directory was empty, so nothing was saved.") ; testing = 1 because it will have .idiot directory
-    :else (println (dir-to-tree-object-contents dir dbase))
+    :else (println (tools/to-hex-string (tools/sha-bytes (dir-to-tree-object-contents dir dbase))))
     ))
